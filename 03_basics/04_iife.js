80cd3fa4ea852b37bf4f396acec78b7a2a345df8
Rahul Kumar <99678937+Rahul-7416@users.noreply.github.com>
2024-02-02 22:30:41 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,9 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-
+// to remove the pollution of global scope, we used iife 
+// format 
+// (whole fuction is now treated as an expression)()
+// same as chai()
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
